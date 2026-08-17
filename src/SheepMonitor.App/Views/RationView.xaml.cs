15dd76964385d4a370edc5fa4986134fc5623e5e
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-18 01:57:48 +0330

    اتصال ذخیره دوره و درصد وعده‌ها

--- src/SheepMonitor.App/Views/RationView.xaml.cs
+++ src/SheepMonitor.App/Views/RationView.xaml.cs
@@ -7,9 +7,10 @@ namespace SheepMonitor.App.Views;
 public partial class RationView : UserControl
 {
     public RationView() => InitializeComponent();
-
     private async void SaveRule_Click(object sender, RoutedEventArgs e) => await RunAsync(vm => vm.SaveRuleAsync());
     private async void Rule_SelectionChanged(object sender, SelectionChangedEventArgs e) => await RunAsync(vm => vm.LoadMealRulesAsync());
+    private async void SaveMealRules_Click(object sender, RoutedEventArgs e) => await RunAsync(vm => vm.SaveMealRulesAsync());
+    private async void SavePeriod_Click(object sender, RoutedEventArgs e) => await RunAsync(vm => vm.SavePeriodAsync());
     private async void CalculateDay_Click(object sender, RoutedEventArgs e) => await RunAsync(vm => vm.CalculateDayAsync());
     private async void CalculatePeriod_Click(object sender, RoutedEventArgs e) => await RunAsync(vm => vm.CalculatePeriodAsync());
 
