f64f0e26c5e3b572a879cd18a9754d2f4ab37610
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-17 23:42:18 +0330

    تکمیل نگاشت Entity Framework Core برای دام، وزن، سلامت و تغذیه

--- src/SheepMonitor.Data/SheepMonitorDbContext.cs
+++ src/SheepMonitor.Data/SheepMonitorDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SheepMonitor.Core.Models;
 
 namespace SheepMonitor.Data;
 
@@ -7,4 +8,42 @@ namespace SheepMonitor.Data;
 /// </summary>
 public sealed class SheepMonitorDbContext(DbContextOptions<SheepMonitorDbContext> options) : DbContext(options)
 {
+    public DbSet<Sheep> Sheep => Set<Sheep>();
+    public DbSet<WeightRecord> WeightRecords => Set<WeightRecord>();
+    public DbSet<HealthRecord> HealthRecords => Set<HealthRecord>();
+    public DbSet<FeedPlan> FeedPlans => Set<FeedPlan>();
+    public DbSet<FeedPlanItem> FeedPlanItems => Set<FeedPlanItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sheep>(e =>
+        {
+            e.ToTable("Sheep"); e.HasKey(x => x.Id); e.HasIndex(x => x.Number).IsUnique();
+            e.Property(x => x.Number).HasMaxLength(50).IsRequired(); e.Property(x => x.Gender).HasMaxLength(20).IsRequired();
+            e.Property(x => x.HealthStatus).HasMaxLength(30).IsRequired(); e.Property(x => x.InitialWeightKg).HasPrecision(8, 2);
+            e.Property(x => x.ImagePath).HasMaxLength(500); e.Property(x => x.Notes).HasMaxLength(2000);
+        });
+        modelBuilder.Entity<WeightRecord>(e =>
+        {
+            e.ToTable("WeightRecords"); e.HasKey(x => x.Id); e.Property(x => x.WeightKg).HasPrecision(8, 2);
+            e.HasIndex(x => new { x.SheepId, x.WeighedAt }); e.HasOne<Sheep>().WithMany().HasForeignKey(x => x.SheepId).OnDelete(DeleteBehavior.Cascade);
+        });
+        modelBuilder.Entity<HealthRecord>(e =>
+        {
+            e.ToTable("HealthRecords"); e.HasKey(x => x.Id); e.Property(x => x.Status).HasMaxLength(30).IsRequired();
+            e.Property(x => x.DiseaseName).HasMaxLength(200); e.Property(x => x.Symptoms).HasMaxLength(2000);
+            e.Property(x => x.Treatment).HasMaxLength(2000); e.Property(x => x.Notes).HasMaxLength(2000);
+            e.HasIndex(x => new { x.SheepId, x.RecordedAt }); e.HasOne<Sheep>().WithMany().HasForeignKey(x => x.SheepId).OnDelete(DeleteBehavior.Cascade);
+        });
+        modelBuilder.Entity<FeedPlan>(e =>
+        {
+            e.ToTable("FeedPlans"); e.HasKey(x => x.Id); e.Property(x => x.Name).HasMaxLength(150).IsRequired();
+            e.Property(x => x.TargetGroup).HasMaxLength(100); e.Property(x => x.Notes).HasMaxLength(2000);
+        });
+        modelBuilder.Entity<FeedPlanItem>(e =>
+        {
+            e.ToTable("FeedPlanItems"); e.HasKey(x => x.Id); e.Property(x => x.FeedName).HasMaxLength(100).IsRequired();
+            e.Property(x => x.AmountKgPerDay).HasPrecision(8, 3); e.HasOne<FeedPlan>().WithMany().HasForeignKey(x => x.FeedPlanId).OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
