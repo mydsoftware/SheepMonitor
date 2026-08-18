88894621de0f39609626d667db74eb5741477a5c
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-18 07:25:28 +0330

    افزودن حالت محاسبه یک گوسفند و میانگین گله

---
+++ src/SheepMonitor.Core/Models/RationCalculationTarget.cs
@@ -0,0 +1,10 @@
+namespace SheepMonitor.Core.Models;
+
+/// <summary>
+/// نوع وزن مبنا برای محاسبه جیره.
+/// </summary>
+public enum RationCalculationTarget
+{
+    SingleSheep = 1,
+    AverageHerdWeight = 2
+}
