3fca259cb68a9cc9c1ccfff52856841796b518d0
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-19 09:28:34 +0330

    feat: ثبت FeedPrice در DbContext

--- src/SheepMonitor.Data/SheepMonitorDbContext.cs
+++ src/SheepMonitor.Data/SheepMonitorDbContext.cs
@@ -19,33 +19,33 @@ public sealed class SheepMonitorDbContext(DbContextOptions<SheepMonitorDbContext
     public DbSet<RationPeriod> RationPeriods => Set<RationPeriod>();
     public DbSet<FeedConsumptionRecord> FeedConsumptionRecords => Set<FeedConsumptionRecord>();
     public DbSet<FeedConsumptionItem> FeedConsumptionItems => Set<FeedConsumptionItem>();
+    public DbSet<FeedPrice> FeedPrices => Set<FeedPrice>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<FeedConsumptionRecord>(e =>
+        modelBuilder.Entity<FeedPrice>(e =>
         {
-            e.ToTable("FeedConsumptionRecords");
+            e.ToTable("FeedPrices");
             e.HasKey(x => x.Id);
             e.Property(x => x.FeedCode).HasMaxLength(100).IsRequired();
-            e.Property(x => x.FeedTitle).HasMaxLength(250).IsRequired();
-            e.Property(x => x.MealCode).HasMaxLength(100).IsRequired();
-            e.Property(x => x.ActualAmountKg).HasPrecision(10, 3);
-            e.Property(x => x.WasteAmountKg).HasPrecision(10, 3);
-            e.Property(x => x.Notes).HasMaxLength(2000);
-            e.HasIndex(x => new { x.ConsumedAt, x.SheepId });
-            e.HasOne<Sheep>().WithMany().HasForeignKey(x => x.SheepId).OnDelete(DeleteBehavior.SetNull);
+            e.Property(x => x.PricePerKg).HasPrecision(18, 2);
+            e.Property(x => x.Currency).HasMaxLength(10).IsRequired();
+            e.HasIndex(x => new { x.FeedCode, x.EffectiveFrom });
+        });
+        modelBuilder.Entity<FeedConsumptionRecord>(e =>
+        {
+            e.ToTable("FeedConsumptionRecords"); e.HasKey(x => x.Id);
+            e.Property(x => x.FeedCode).HasMaxLength(100).IsRequired(); e.Property(x => x.FeedTitle).HasMaxLength(250).IsRequired();
+            e.Property(x => x.MealCode).HasMaxLength(100).IsRequired(); e.Property(x => x.ActualAmountKg).HasPrecision(10, 3);
+            e.Property(x => x.WasteAmountKg).HasPrecision(10, 3); e.Property(x => x.Notes).HasMaxLength(2000);
+            e.HasIndex(x => new { x.ConsumedAt, x.SheepId }); e.HasOne<Sheep>().WithMany().HasForeignKey(x => x.SheepId).OnDelete(DeleteBehavior.SetNull);
         });
         modelBuilder.Entity<FeedConsumptionItem>(e =>
         {
-            e.ToTable("FeedConsumptionItems");
-            e.HasKey(x => x.Id);
-            e.Property(x => x.FeedCode).HasMaxLength(100).IsRequired();
-            e.Property(x => x.PlannedKg).HasPrecision(10, 3);
-            e.Property(x => x.ActualKg).HasPrecision(10, 3);
-            e.Property(x => x.WasteKg).HasPrecision(10, 3);
-            e.HasIndex(x => new { x.FeedConsumptionRecordId, x.FeedCode });
-            e.HasOne<FeedConsumptionRecord>().WithMany().HasForeignKey(x => x.FeedConsumptionRecordId).OnDelete(DeleteBehavior.Cascade);
+            e.ToTable("FeedConsumptionItems"); e.HasKey(x => x.Id); e.Property(x => x.FeedCode).HasMaxLength(100).IsRequired();
+            e.Property(x => x.PlannedKg).HasPrecision(10, 3); e.Property(x => x.ActualKg).HasPrecision(10, 3); e.Property(x => x.WasteKg).HasPrecision(10, 3);
+            e.HasIndex(x => new { x.FeedConsumptionRecordId, x.FeedCode }); e.HasOne<FeedConsumptionRecord>().WithMany().HasForeignKey(x => x.FeedConsumptionRecordId).OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
