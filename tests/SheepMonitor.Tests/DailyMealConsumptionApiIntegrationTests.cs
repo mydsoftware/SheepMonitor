b408cf0dc25de86ad5874a45101dd145d58b5419
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-08-20 14:22:17 +0330

    fix: افزودن DependencyInjection برای تست Integration

--- tests/SheepMonitor.Tests/DailyMealConsumptionApiIntegrationTests.cs
+++ tests/SheepMonitor.Tests/DailyMealConsumptionApiIntegrationTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using SheepMonitor.Api;
 using SheepMonitor.Core.Models;
 using SheepMonitor.Data;
